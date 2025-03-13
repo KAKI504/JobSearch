f8733c8f1e3dab7939962843fad213d54501bed5
kanat <esenalievkanat.90@gmail.com>
2025-03-13 20:53:20 +0600

    Добавлена модель для хранения информации о вакансиях

---
+++ src/main/java/org/example/jobsearch/model/Vacancy.java
@@ -0,0 +1,47 @@
+package org.example.jobsearch.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "vacancies")
+public class Vacancy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private BigDecimal salary;
+
+    @Column(name = "exp_from")
+    private Integer expFrom;
+
+    @Column(name = "exp_to")
+    private Integer expTo;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Column(name = "update_time")
+    private LocalDateTime updateTime;
+
+    @OneToMany(mappedBy = "vacancy", cascade = CascadeType.ALL)
+    private List<RespondedApplicant> responses;
+}
